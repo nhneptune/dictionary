5bd130c92eec37c374bd1ef370e7d31f26d559f6
nhneptune <124415548+nhneptune@users.noreply.github.com>
2023-11-07 14:20:18 +0700

    Initial commit

---
+++ src/main/java/com/example/dictionary/HelloApplication.java
@@ -0,0 +1,17 @@
+package com.example.dictionary;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+  @Override
+
+  public static void main(String[] args) {
+     launch();
+  }
+}
