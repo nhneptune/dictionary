9f151cfe0cb9ff386ce874f34d9189a379e8105f
nhneptune <124415548+nhneptune@users.noreply.github.com>
2023-11-11 16:25:17 +0700

    hehe hom truoc t xoa nham

--- src/main/java/com/example/dictionary/HelloApplication.java
+++ src/main/java/com/example/dictionary/HelloApplication.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-
+ //111
   @Override
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
